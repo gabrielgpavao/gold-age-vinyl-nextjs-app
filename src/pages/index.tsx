3b1acaba22382dfbb688085aa6cae95c5bbc1b18
Gabriel Pavão <gabpavao@outlook.com>
2023-04-15 15:52:37 -0300

    feat: Header component and its style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,18 @@
 import Head from 'next/head'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import Header from '@/components/Header/Header'
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Gold Age Vinyl</title>
-		<meta charSet='UTF-8'/>
-        <meta name="description" content="E-commerce de Discos de Vinis para amantes da Era de Ouro do Rock" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.png" />
-      </Head>
-	</>
-  )
+	return (
+		<>
+			<Head>
+				<title>Gold Age Vinyl</title>
+				<meta charSet='UTF-8'/>
+				<meta name="description" content="E-commerce de Discos de Vinis para amantes da Era de Ouro do Rock" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.png" />
+			</Head>
+
+			<Header/>
+		</>
+	)
 }
